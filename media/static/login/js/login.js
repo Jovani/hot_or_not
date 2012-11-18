0194b2961e646883c97c80b8f9e33b977152b06e
Ivan Djokic <jovani.ivan@gmail.com>
2012-11-17 19:53:05 -0500

    type change

--- media/static/login/js/login.js
+++ media/static/login/js/login.js
@@ -1,1 +1,13 @@
-/Users/ivand/Documents/hot_or_not/login/static/login/js/login.js
+$(document).ready(function() {
+    _500px.init({
+        sdk_key: '5318bce8cc510bcc7f48a74feab6d6bc18d7d944'
+    });
+
+    _500px.on('authorization_obtained', function() {
+        $('not_logged_in').hide();
+        $('#logged_in').show();
+    })
+    _500px.getAuthorizationStatus();
+
+    $('#login').click(_500px.login);
+});
