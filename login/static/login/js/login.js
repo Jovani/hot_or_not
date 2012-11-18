8da4d0f2a8334f6b29d8e4459bfb960d110f7f13
Ivan Djokic <jovani.ivan@gmail.com>
2012-11-18 10:48:51 -0500

    login test 2

--- login/static/login/js/login.js
+++ login/static/login/js/login.js
@@ -3,15 +3,25 @@ $(function() {
         sdk_key: '5318bce8cc510bcc7f48a74feab6d6bc18d7d944'
     });
 
-    _500px.on('authorization_obtained', function() {
-        // window.location.href = 'http://www.google.ca'
-        console.log(self);
-        _500px.api('/users', function (response) {
-        	console.log(response.user);
-        });
+    // _500px.on('authorization_obtained', function() {
+    //     // window.location.href = 'http://www.google.ca'
+    //     console.log(self);
+    //     _500px.api('/users', function (response) {
+    //     	console.log(response.user);
+    //     });
 
-    });
+    // });
     _500px.getAuthorizationStatus();
 
-    $('#login').click(_500px.login);
+    $('#login').click(_500px.login(function (status) {
+    	if (status === 'authorized') {
+			_500px.api('/users', function (response) {
+				console.log(response.user);
+			});
+    	} else {
+    		console.log('Not logged in');
+    	}
+
+
+    }));
 });
