336ef4a22cbc3be8c66274b235e967edcf88020e
Ivan Djokic <jovani.ivan@gmail.com>
2012-11-17 20:13:10 -0500

    static

--- hot_or_not/settings.py
+++ hot_or_not/settings.py
@@ -68,6 +68,7 @@ STATIC_URL = '/media/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(MEDIA_ROOT, 'common'),
+    os.path.join(MEDIA_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
