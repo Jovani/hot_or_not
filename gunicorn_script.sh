0ea01dab2c739fbf985b8f1c382ac1598c6be58d
Bartek Ciszkowski <bart.ciszk@gmail.com>
2012-11-17 23:42:45 -0500

    IT WOOOORKSgit statusgit status

--- gunicorn_script.sh
+++ gunicorn_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-LOGFILE=~/var/log/gunicorn/hot_or_not.log
+LOGFILE=/var/log/gunicorn/hot_or_not.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=3
 # user/group to run as
-USER=ivan
-GROUP=ivan
-cd /home/ivan/hot_or_not
-source /home/ivan/Envs/ivan_pixelhack/bin/activate
+USER=admin
+GROUP=admin
+cd /home/admin/apps/hot_or_not
+source /home/admin/.virtualenvs/ivan_pixelhack/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec ../bin/gunicorn_django -b 127.0.0.1:4000 -w $NUM_WORKERS \
+exec gunicorn_django -b 127.0.0.1:8035 -w $NUM_WORKERS \
   --user=$USER --group=$GROUP --log-level=debug \
   --log-file=$LOGFILE 2>>$LOGFILE
